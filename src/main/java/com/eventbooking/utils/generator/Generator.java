5f6a523025d6a8874513028bd687a6932af075f8
root <root>
2018-05-10 21:40:37 +0800

    //日常提交

--- src/main/java/com/eventbooking/utils/generator/Generator.java
+++ src/main/java/com/eventbooking/utils/generator/Generator.java
@@ -5,6 +5,7 @@ import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,8 +14,9 @@ public class Generator{
 	public static void main(String[] args) throws Exception {
 		List<String> warnings = new ArrayList<String>();
 		boolean overwrite = true;
+		File configFile = new File("D:\\IdeaProjects\\EventBooking\\src\\main\\java\\com\\eventbooking\\utils\\generator\\generator.xml");
 		ConfigurationParser cp = new ConfigurationParser(warnings);
-		Configuration config = cp.parseConfiguration(Generator.class.getResourceAsStream("generator.xml"));
+		Configuration config = cp.parseConfiguration(configFile);
 		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
 		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
 		myBatisGenerator.generate(null);
